780cce2e83b09e598c9b348a5c352b7fd09f8516
ferdinand <you@example.com>
2018-03-13 12:50:02 +0700

    revisi add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def helo
-    render html : "hello, World!"
+  def hello
+    render html: "hello, World!"
   end
 end
